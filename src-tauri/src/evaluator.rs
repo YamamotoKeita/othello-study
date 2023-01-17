90a1e890f32f92b2cd9b7f2bce0fdfa3681b8109
山本敬太 <yamamoto@altonotes.co.jp>
2023-01-17 15:04:57 +0900

    Add comments

--- src-tauri/src/evaluator.rs
+++ src-tauri/src/evaluator.rs
@@ -11,9 +11,15 @@ pub trait Evaluator {
     /*
      * Evaluates a board from a standpoint of "BoardNode.player_stones".
      *
-     * If player_stones have the advantage, the evaluation is a positive number.
-     * If opponent_stones have the advantage, the evaluation is a negative number.
-     * If player_stones and opponent_stones are equal in the advantage, the evaluation is zero.
+     * # Sings of number
+     * - If player_stones have the advantage, the evaluation is a positive number.
+     * - If opponent_stones have the advantage, the evaluation is a negative number.
+     * - If player_stones and opponent_stones are equal in the advantage, the evaluation is zero.
+     *
+     * # Values of number
+     * Evaluation values are equivalent to stone number in the end of game.
+     * For example, Victory with difference of 10 stones corresponds to 10, and defeat with difference of 10 stones corresponds to -10.
+     * They will be between -64 and 64.
      *
      */
     fn evaluate(&self, board: &BoardNode) -> Evaluation;
